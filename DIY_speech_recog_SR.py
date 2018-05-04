9f47ae95c0560c67354e24bac2a8ba0bd6a06b0c
Mr. Rosenfeld <srosenfeld@users.noreply.github.com>
2018-05-04 09:47:45 -0400

    Update DIY_speech_recog_SR.py

--- DIY_speech_recog_SR.py
+++ DIY_speech_recog_SR.py
@@ -2,7 +2,7 @@
 #This source code comes from the project: https://github.com/Uberi/speech_recognition/blob/master/examples/microphone_recognition.py
 #For reference: https://pypi.org/project/SpeechRecognition/
 
-#You'll need to run 'pip install SpeechRecognition' to make this work
+#You'll need to run 'pip install SpeechRecognition' and 'pip install PyAudio' to make this work
 
 # NOTE: this example requires PyAudio because it uses the Microphone class
 import win32com.client as wincl
